88d96e065b69924501dcb2d8bc3a26b5d10f41a4
Oumar Zouma <zoumarbalde@gmail.com>
2025-11-17 15:31:21 -0500

    Voici mon Exercice_7

--- A_Exo7.dart
+++ A_Exo7.dart
@@ -8,9 +8,9 @@ class Tache {
 }
 
 void main() {
-  var t1 = Tache('Faire les courses');
-  var t2 = Tache('Réviser Dart');
-  var t3 = Tache('Envoyer le devoir');
+  var t1 = Tache('Je fais mes courses');
+  var t2 = Tache('Je réviser Dart pour mon QCM');
+  var t3 = Tache('envoyer le devoir de developpement application');
 
   print('Nombre total de tâches : ${Tache.nombreTotal}');
 }
